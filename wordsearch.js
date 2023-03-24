03c5cf520a64607c45f2b4b3e0972e5efe7820c7
Anthony Chambers <ajfchambers@gmail.com>
2023-03-24 18:37:27 -0400

    refactor variable names

--- wordsearch.js
+++ wordsearch.js
@@ -1,5 +1,4 @@
 const wordSearch = function(letters, word) {
-  let wordChoice = word;
 
   // Checks empty array, 1st priority
   if (letters.length === 0) {
@@ -9,39 +8,39 @@ const wordSearch = function(letters, word) {
   // Horizontal search, 2nd priority
   const horizontalJoin = letters.map((ls) => ls.join(""));
   for (const l of horizontalJoin) {
-    if (l.includes(wordChoice)) return true;
+    if (l.includes(word)) return true;
   }
 
   // Vertical search, 3rd priority
   const vertMatrix = transpose(letters);
   const vertJoin = vertMatrix.map((ls) => ls.join(""));
   for (const l of vertJoin) {
-    if (l.includes(wordChoice)) return true;
+    if (l.includes(word)) return true;
   }
 
   // Diagonal search first half of array, 4th priority
   const diagJoinLeft = transposeLeft(letters).map((ls) => ls.join(""));
   for (const l of diagJoinLeft) {
-    if (l.includes(wordChoice)) return true;
+    if (l.includes(word)) return true;
   }
 
   // Diagonal search second half of array, 5th priority
   const diagJoinRight = transposeRight(letters).map((ls) => ls.join(""));
   for (const l of diagJoinRight) {
-    if (l.includes(wordChoice)) return true;
+    if (l.includes(word)) return true;
   }
     
   // Horizontal backwards search, 6th priority
-  wordChoice = word.split("").reverse().join("");
+  const wordReverse = word.split("").reverse().join("");
   const horizontalJoinRev = letters.map((ls) => ls.join(""));
   for (const l of horizontalJoinRev) {
-    if (l.includes(wordChoice)) return true;
+    if (l.includes(wordReverse)) return true;
   }
 
   // Vertical backwards search, 7th priority
   const vertJoinRev = vertMatrix.map((ls) => ls.join(""));
   for (const l of vertJoinRev) {
-    if (l.includes(wordChoice)) return true;
+    if (l.includes(wordReverse)) return true;
   }
   
   return false;
@@ -62,6 +61,7 @@ const transpose = function(matrix) {
   return newMatrix;
 };
 
+// Tranponse top half (right side) of matrix
 const transposeRight = function(matrix) {
   const newMatrix = [];
   for (let i = 0; i < matrix[0].length; i++) {
@@ -73,6 +73,7 @@ const transposeRight = function(matrix) {
   return newMatrix;
 };
 
+// Tranpose bottom half (left side) of matrix
 const transposeLeft = function(matrix) {
   const newMatrix = [];
   for (let i = 0; i < matrix[0].length; i++) {
