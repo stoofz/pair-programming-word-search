dbe825fe9b36bb284f9b971ce69483681f4c184f
Anthony Chambers <ajfchambers@gmail.com>
2023-03-24 16:03:31 -0400

    logic refactored

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,6 +3,18 @@ const assert = chai.assert;
 
 const { wordSearch } = require("../wordsearch.js");
 
+const origArray = [
+  ["A", "W", "C", "F", "Q", "U", "A", "L"],
+  ["S", "E", "I", "N", "F", "E", "L", "D"],
+  ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+  ["H", "M", "J", "T", "E", "V", "R", "G"],
+  ["W", "H", "C", "S", "Y", "E", "R", "L"],
+  ["B", "F", "R", "E", "N", "E", "Y", "B"],
+  ["U", "B", "T", "W", "A", "P", "A", "I"],
+  ["O", "D", "C", "A", "K", "U", "A", "S"],
+  ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+];
+
 const newArray = [
   ["S", "W", "C", "F", "Q", "U", "A", "L"],
   ["S", "X", "I", "N", "F", "E", "L", "D"],
@@ -15,63 +27,47 @@ const newArray = [
   ["D", "Z", "K", "F", "Q", "U", "A", "L"],
 ];
 const revArray = [
-  ["S", "W", "C", "F", "Q", "U", "A", "L"],
+  ["S", "D", "C", "F", "Q", "U", "A", "L"],
   ["D", "L", "E", "F", "N", "I", "E", "S"],
-  ["E", "F", "C", "F", "Q", "U", "A", "L"],
-  ["I", "M", "J", "T", "E", "V", "R", "G"],
-  ["N", "H", "C", "S", "Y", "E", "R", "L"],
-  ["F", "F", "R", "E", "N", "E", "Y", "B"],
-  ["E", "B", "T", "W", "A", "P", "A", "I"],
-  ["L", "D", "C", "A", "K", "U", "A", "S"],
+  ["E", "E", "C", "F", "Q", "U", "A", "L"],
+  ["I", "F", "J", "T", "E", "V", "R", "G"],
+  ["N", "N", "C", "S", "Y", "E", "R", "L"],
+  ["F", "I", "R", "E", "N", "E", "Y", "B"],
+  ["E", "E", "T", "W", "A", "P", "A", "I"],
+  ["L", "S", "C", "A", "K", "U", "A", "S"],
+  ["D", "Z", "K", "F", "Q", "U", "A", "L"],
+];
+
+const revArray2 =  [
+  ["S", "D", "C", "F", "Q", "U", "A", "L"],
+  ["D", "L", "E", "F", "N", "T", "E", "S"],
+  ["E", "E", "C", "F", "Q", "U", "A", "L"],
+  ["I", "F", "J", "T", "E", "V", "R", "G"],
+  ["N", "N", "C", "S", "Y", "E", "R", "L"],
+  ["F", "I", "R", "E", "N", "E", "Y", "B"],
+  ["E", "E", "T", "W", "A", "P", "A", "I"],
+  ["L", "S", "C", "A", "K", "U", "A", "S"],
   ["D", "Z", "K", "F", "Q", "U", "A", "L"],
 ];
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
-    const result = wordSearch(
-      [
-        ["A", "W", "C", "F", "Q", "U", "A", "L"],
-        ["S", "E", "I", "N", "F", "E", "L", "D"],
-        ["Y", "F", "C", "F", "Q", "U", "A", "L"],
-        ["H", "M", "J", "T", "E", "V", "R", "G"],
-        ["W", "H", "C", "S", "Y", "E", "R", "L"],
-        ["B", "F", "R", "E", "N", "E", "Y", "B"],
-        ["U", "B", "T", "W", "A", "P", "A", "I"],
-        ["O", "D", "C", "A", "K", "U", "A", "S"],
-        ["E", "Z", "K", "F", "Q", "U", "A", "L"],
-      ],
-      "FRANK"
-    );
-
+    const result = wordSearch(origArray, "FRANK");
     assert.isFalse(result);
   });
 
   it("should return true if the word is present horizontally", function() {
-    const result = wordSearch(
-      [
-        ["A", "W", "C", "F", "Q", "U", "A", "L"],
-        ["S", "E", "I", "N", "F", "E", "L", "D"],
-        ["Y", "F", "C", "F", "Q", "U", "A", "L"],
-        ["H", "M", "J", "T", "E", "V", "R", "G"],
-        ["W", "H", "C", "S", "Y", "E", "R", "L"],
-        ["B", "F", "R", "E", "N", "E", "Y", "B"],
-        ["U", "B", "T", "W", "A", "P", "A", "I"],
-        ["O", "D", "C", "A", "K", "U", "A", "S"],
-        ["E", "Z", "K", "F", "Q", "U", "A", "L"],
-      ],
-      "SEINFELD"
-    );
-
+    const result = wordSearch(origArray, "SEINFELD");
     assert.isTrue(result);
   });
 
   it("should return true if the word is present vertically", function() {
-    const result = wordSearch(newArray, "SEINFELD", true);
+    const result = wordSearch(newArray, "SEINFELD");
     assert.isTrue(result);
   });
 
   it("should return false if the word is NOT present vertically", function() {
-    const result = wordSearch(newArray, "POPEYE", true);
+    const result = wordSearch(newArray, "POPEYE");
     assert.isFalse(result);
   });
 
@@ -80,8 +76,19 @@ describe("#wordSearch()", function() {
     assert.isFalse(results);
   });
 
-  it("should return true if word is present in reverse", function() {
-    const results = wordSearch(revArray, "SEINFELD", false, true);
+  it("should return true if word is present in reverse horizontally", function() {
+    const results = wordSearch(revArray, "SEINFELD");
     assert.isTrue(results);
   });
+
+  it("should return true if word is present in reverse vertically", function() {
+    const results = wordSearch(revArray2, "SEINFELD");
+    assert.isTrue(results);
+  });
+
+  it("should return false if word is NOT present in reverse vertically", function() {
+    const results = wordSearch(revArray, "TOBY");
+    assert.isFalse(results);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,19 +1,39 @@
-const wordSearch = function(letters, word, vertChoice = false, reverseChoice = false) {
+const wordSearch = function(letters, word) {
   let matrix = letters;
   let wordChoice = word;
 
-  if (reverseChoice) {
-    wordChoice = word.split("").reverse().join("");
+  // Checks empty array, 1st priority
+  if (letters.length === 0) {
+    return false;
   }
-    
-  if (vertChoice) {
-    matrix = transpose(letters);
-  }
-    
+
+  // Horizontal search, 2nd priority
   const horizontalJoin = matrix.map((ls) => ls.join(""));
   for (const l of horizontalJoin) {
     if (l.includes(wordChoice)) return true;
   }
+
+  // Vertical search, 3rd priority
+  matrix = transpose(letters);
+  const vertJoin = matrix.map((ls) => ls.join(""));
+  for (const l of vertJoin) {
+    if (l.includes(wordChoice)) return true;
+  }
+
+  // Horizontal backwards search, 4th priority
+  wordChoice = word.split("").reverse().join("");
+  const horizontalJoinRev = matrix.map((ls) => ls.join(""));
+  for (const l of horizontalJoinRev) {
+    if (l.includes(wordChoice)) return true;
+  }
+
+  // Vertical backwards search, 5th priority
+  matrix = transpose(letters);
+  const vertJoinRev = matrix.map((ls) => ls.join(""));
+  for (const l of vertJoinRev) {
+    if (l.includes(wordChoice)) return true;
+  }
+  
   return false;
 };
 
