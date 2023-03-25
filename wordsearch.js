18a17db5902f302b771d0cd1a958ff7cfcef6275
Anthony Chambers <ajfchambers@gmail.com>
2023-03-24 21:31:37 -0400

    add reusable loop search function

--- wordsearch.js
+++ wordsearch.js
@@ -1,5 +1,5 @@
 const wordSearch = function(letters, word) {
-
+  
   // Checks empty array, 1st priority
   if (letters.length === 0) {
     return false;
@@ -7,79 +7,71 @@ const wordSearch = function(letters, word) {
 
   // Horizontal search, 2nd priority
   const horizontalJoin = letters.map((ls) => ls.join(""));
-  for (const l of horizontalJoin) {
-    if (l.includes(word)) return true;
+  if (findWord(horizontalJoin, word) === true) {
+    return true;
   }
 
   // Vertical search, 3rd priority
-  const vertMatrix = transpose(letters);
-  const vertJoin = vertMatrix.map((ls) => ls.join(""));
-  for (const l of vertJoin) {
-    if (l.includes(word)) return true;
+  const vertJoin = transposeChoice(letters).map((ls) => ls.join(""));
+  if (findWord(vertJoin, word) === true) {
+    return true;
   }
 
   // Diagonal search first half of array, 4th priority
-  const diagJoinLeft = transposeLeft(letters).map((ls) => ls.join(""));
-  for (const l of diagJoinLeft) {
-    if (l.includes(word)) return true;
+  const diagJoinLeft = transposeChoice(letters, "diaL").map((ls) => ls.join(""));
+  if (findWord(diagJoinLeft, word) === true) {
+    return true;
   }
 
   // Diagonal search second half of array, 5th priority
-  const diagJoinRight = transposeRight(letters).map((ls) => ls.join(""));
-  for (const l of diagJoinRight) {
-    if (l.includes(word)) return true;
+  const diagJoinRight = transposeChoice(letters, "diaR").map((ls) => ls.join(""));
+  if (findWord(diagJoinRight, word) === true) {
+    return true;
   }
-    
-  // Horizontal backwards search, 6th priority
+  
   const wordReverse = word.split("").reverse().join("");
+
+  // Horizontal backwards search, 6th priority
   const horizontalJoinRev = letters.map((ls) => ls.join(""));
-  for (const l of horizontalJoinRev) {
-    if (l.includes(wordReverse)) return true;
+  if (findWord(horizontalJoinRev, wordReverse) === true) {
+    return true;
   }
 
   // Vertical backwards search, 7th priority
-  const vertJoinRev = vertMatrix.map((ls) => ls.join(""));
-  for (const l of vertJoinRev) {
-    if (l.includes(wordReverse)) return true;
+  const vertJoinRev = transposeChoice(letters).map((ls) => ls.join(""));
+  if (findWord(vertJoinRev, wordReverse) === true) {
+    return true;
   }
   
+  //console.log("Nothing found");
   return false;
 };
 
-const transpose = function(matrix) {
-  const newMatrix = [];
-  //loop through the length of the first array
-  for (let i = 0; i < matrix[0].length; i++) {
-    //push to new array to set length(rows) based on input columns
-    newMatrix.push([]);
-    //loop through items of each nested array
-    for (let j = 0; j < matrix.length; j++) {
-      //push column values into nested arrays (rows)
-      newMatrix[i].push(matrix[j][i]);
+
+// Find word search loop
+const findWord = function(arrayMatrix, word) {
+  //console.log("searching for " + word);
+  for (const l of arrayMatrix) {
+    if (l.includes(word)) {
+      //console.log("Found " + l);
+      return true;
     }
   }
-  return newMatrix;
 };
 
-// Tranponse top half (right side) of matrix
-const transposeRight = function(matrix) {
+
+const transposeChoice = function(matrix, choice = "reg") {
   const newMatrix = [];
   for (let i = 0; i < matrix[0].length; i++) {
     newMatrix.push([]);
     for (let j = 0; j < matrix.length; j++) {
-      newMatrix[i].push(matrix[j][j - i]);
-    }
-  }
-  return newMatrix;
-};
-
-// Tranpose bottom half (left side) of matrix
-const transposeLeft = function(matrix) {
-  const newMatrix = [];
-  for (let i = 0; i < matrix[0].length; i++) {
-    newMatrix.push([]);
-    for (let j = 0; j < matrix.length; j++) {
-      newMatrix[i].push(matrix[j][j + i]);
+      if (choice === "reg") {
+        newMatrix[i].push(matrix[j][i]);
+      } else if (choice === "diaR") {
+        newMatrix[i].push(matrix[j][j - i]);
+      } else if (choice === "diaL") {
+        newMatrix[i].push(matrix[j][j + i]);
+      }
     }
   }
   return newMatrix;
